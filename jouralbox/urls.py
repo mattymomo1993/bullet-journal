6f53e7f4bfa7b852387ddb620026b555f4372c40
Tim La <email@example.com>
2021-02-06 11:17:14 -0500

    added media and static url

--- jouralbox/urls.py
+++ jouralbox/urls.py
@@ -24,6 +24,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
     path('', animation),
     path('home/', home, name="home"),
     path('login/', index, name="login"),
